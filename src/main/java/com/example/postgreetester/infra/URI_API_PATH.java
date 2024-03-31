5b043faa8d73292675c9e630415466500b0844c2
vinicius89stna <vinicius_89st@hotmail.com>
2024-03-31 19:19:01 -0300

    FIX: marge conflict

--- src/main/java/com/example/postgreetester/infra/URI_API_PATH.java
+++ src/main/java/com/example/postgreetester/infra/URI_API_PATH.java
@@ -4,9 +4,5 @@ public class URI_API_PATH {
 
     public final static String API = "/api";
     public final static String VERSION = "/v1";
-<<<<<<< HEAD
     public final static String USER_API = API + VERSION + "/users";
-=======
-    public final static String ALUMNS_API = API + VERSION + "/alunos";
->>>>>>> cfc82032a91ce96c00a8eb9a0223834313308dd0
 }
